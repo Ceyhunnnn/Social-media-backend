33cc4718693a258843e1e4c072603bb83e2f0786
Ceyhun-gl <ceyhungul148@gmail.com>
2024-06-12 17:21:17 +0300

    auth controller, model and middleware completed

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,5 +1,37 @@
-const login = async (req, res) => {};
-const register = async (req, res) => {};
+const user = require("../models/userModel");
+const APIError = require("../utils/error");
+const Response = require("../utils/response");
+const bcrypt = require("bcrypt");
+const { createToken } = require("../middlewares/auth");
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const checkUser = await user.findOne({ email });
+  if (checkUser) {
+    throw new APIError("E-mail already in use", 400);
+  }
+  req.body.password = await bcrypt.hash(req.body.password, 10);
+  const createdUser = new user(req.body);
+  await createdUser
+    .save()
+    .then((data) => new Response(null, "Account created").created(res))
+    .catch(() => new APIError("Account could not be created", 400));
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const loginUser = await user.findOne({ email });
+  if (!loginUser) {
+    throw new APIError("Email or password wrong, please Please try again", 401);
+  }
+  if (!loginUser.isActive) {
+    throw new APIError("user account is deactive", 400);
+  }
+  const comparePassword = await bcrypt.compare(password, loginUser.password);
+  if (!comparePassword) {
+    throw new APIError("Email or password wrong, please Please try again", 401);
+  }
+  createToken(loginUser, res);
+};
 
 module.exports = {
   login,
---
+++ src/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("users", userSchema);
+
+module.exports = user;
